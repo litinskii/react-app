c1370da426d94cf1b9d7e6d7cb50b990e765e1e6
w.siteee <w.siteee@gmail.com>
2018-04-06 23:59:48 +0300

    react-app : REST / API - fix eslint rule - add example how to fetch data

--- src/application/components/SecondPage/index.js
+++ src/application/components/SecondPage/index.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { sum } from "lodash";
 import "./SecondPage.scss";
 import globalStore from "../globalStore";
 import withStore from "../../common/withStore";
+import { getHomePageClicksCount } from "../../api/settingsHomePage";
+import { getSomeComponentInnerClicksCount } from "../../api/settingsSomeComponentInner";
+import LoaderOverlay from "../LoaderOverlay";
 
-const SecondPage = ({ clickCount }) => (
-  <div className="SecondPage">
-    <div className="SecondPage__clicks">{`All time HomePage and SomeComponentInner clicks: ${clickCount}`}</div>
-  </div>
-);
+class SecondPage extends Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    try {
+      globalStore.setClicksCount(sum(await Promise.all([getHomePageClicksCount(), getSomeComponentInnerClicksCount()])));
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  render() {
+    const { clickCount } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="SecondPage">
+        {loading ? <LoaderOverlay /> : undefined}
+        <div className="SecondPage__clicks">{`All time HomePage and SomeComponentInner clicks: ${clickCount}`}</div>
+      </div>
+    );
+  }
+}
 
 SecondPage.propTypes = {
   clickCount: PropTypes.number.isRequired
